52165cc4d4c84928f507588fa191cbfec0a24522
farid1990 <faridbhr@gmail.com>
2023-09-05 20:55:51 +0330

    recipe-list html and css + creating recipe.model and ingredient.model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes : Recipe[] =[
+    new Recipe('Test Recipe' 
+    , 'for Test Description part of Recipe' 
+    ,'https://www.aspicyperspective.com/wp-content/uploads/2020/07/best-hamburger-patties-1.jpg')
+    ,new Recipe('Test Recipe2' 
+    , 'for Test Description part of Recipe2' 
+    ,'https://www.aspicyperspective.com/wp-content/uploads/2020/07/best-hamburger-patties-1.jpg')
+  ]
 }
