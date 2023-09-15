1212bc529a29491ec698dee27f1e74791fd0309a
farid1990 <faridbhr@gmail.com>
2023-09-15 16:47:40 +0330

    adding router to app

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,34 +1,38 @@
-import { Component , Input} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ActivatedRoute , Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
-import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
+
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
-  @Input() recipe !:Recipe ;
+export class RecipeDetailComponent implements OnInit{
+   recipe !:Recipe ;
+   id !: number ;
 
-  constructor(private recipeService:RecipeService){}
+  constructor(private recipeService:RecipeService 
+    ,private router : Router
+    , private route : ActivatedRoute){}
   
-  isDropdownOpen: boolean = false;
-  isFetchDataActive: boolean = false;
 
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
+  ngOnInit(){
+    this.route.params.subscribe(
+      (params : Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    )
   }
-
-  activateFetchData() {
-    this.isFetchDataActive = true;
-  }
-
-  deactivateFetchData() {
-    this.isFetchDataActive = false;
-  }
-
+  
   onAddToShoppingList(){
      this.recipeService.addIngredients(this.recipe.ingredients);
+     this.router.navigate(['/shopping-list']);
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'] , {relativeTo:this.route});
   }
 }
