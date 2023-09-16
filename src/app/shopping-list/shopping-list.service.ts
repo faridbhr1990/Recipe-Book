464d49b7437ad6bc7480372ddcff57b7ac2d35a3
farid1990 <faridbhr@gmail.com>
2023-09-16 18:05:44 +0330

    Giving Better Aproach by using Observables

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,11 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from "rxjs";
 
 
 @Injectable()
 export class ShoppingListService{
-    ingredientChanged = new EventEmitter<Ingredient[]>();
+    ingredientChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] =[
         new Ingredient('Apple' , 5),
@@ -17,12 +18,12 @@ export class ShoppingListService{
       
       addIngredient(ingredient : Ingredient){
         this.ingredients.push(ingredient);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
       }
 
       addIngredients(ingredients : Ingredient[]){
         this.ingredients.push(...ingredients);
-        this.ingredientChanged.emit(this.ingredients.slice());
+        this.ingredientChanged.next(this.ingredients.slice());
       }
 
 
